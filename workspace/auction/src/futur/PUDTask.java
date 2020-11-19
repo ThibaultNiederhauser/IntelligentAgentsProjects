63abf51294aa48e4babae5da2467f0aee9424296
Thibault Niederhauser <thibault.niederhauser@epfl.ch>
2020-11-19 23:55:13 +0100

    use proba dist (messy version)

---
+++ workspace/auction/src/futur/PUDTask.java
@@ -0,0 +1,14 @@
+package AuctionFutur;
+
+import logist.task.Task;
+
+
+public class PUDTask {
+    public Task task;
+    public String type;
+
+    public PUDTask(Task task_input, String type_input) {
+        this.task = task_input;
+        this.type = type_input;
+    }
+}
